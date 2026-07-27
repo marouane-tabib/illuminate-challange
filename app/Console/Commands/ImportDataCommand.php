7ad9420999f9ddc3b9a071045710204c66313d58
marouane-tabib <tabibmarouane6@gmail.com>
2026-07-27 06:17:45 +0100

    database_export | Add Error handling in the ImportDataCommand processes

--- app/Console/Commands/ImportDataCommand.php
+++ app/Console/Commands/ImportDataCommand.php
@@ -8,6 +8,7 @@ use App\Models\Neighborhood;
 use App\Repositories\IncidentPgRepository;
 use App\Repositories\NeighborhoodPgRepository;
 use Illuminate\Console\Command;
+use Throwable;
 
 class ImportDataCommand extends Command
 {
@@ -30,16 +31,25 @@ class ImportDataCommand extends Command
      */    
     public function handle()
     {
-        $this->importNeighborhoods();
-        $this->info('Neighborhoods imported.');
+        try {
+            $this->importNeighborhoods();
+            $this->info('Neighborhoods imported.');
 
-        $this->importIncidents();
-        $this->info('Incidents imported.');
+            $this->importIncidents();
+            $this->info('Incidents imported.');
+        } catch (Throwable $e) {
+            logger()->error('ImportDataCommand failed', ['exception' => $e]);
+            $this->fail('Import failed...');
+        }
     }
 
     private function importNeighborhoods(): void
     {
         $neighborhoods = NeighborhoodPgRepository::all();
+
+        if (empty($neighborhoods)) {
+            throw new \RuntimeException('No neighborhoods found in remote database.');
+        }
 
         foreach ($neighborhoods as $nb) {
             $centroid = NeighborhoodHelper::polygonCentroid($nb->boundary);
@@ -54,7 +64,12 @@ class ImportDataCommand extends Command
 
     private function importIncidents(): void
     {
-        $incidentsData = IncidentPgRepository::all();            
+        $incidentsData = IncidentPgRepository::all();
+
+        if (empty($incidentsData)) {
+            throw new \RuntimeException('No incidents found in remote database.');
+        }
+
         $incidentsArray = array_map(fn($row) => (array) $row, $incidentsData);
         Incident::insert($incidentsArray);
     }
