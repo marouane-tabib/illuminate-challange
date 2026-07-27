ea361145a22c07fee1463b9b0fb5ac8945932fae
marouane-tabib <tabibmarouane6@gmail.com>
2026-07-27 19:59:04 +0100

    database_export | Add incidents relationship to Neighborhood model class

--- app/Models/Neighborhood.php
+++ app/Models/Neighborhood.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Relations\DonutRelation;
 use Illuminate\Database\Eloquent\Model;
 
 class Neighborhood extends Model
 {
     protected $fillable = ['name', 'centroid_lat', 'centroid_lng'];
 
+    
+    public function incidents(): DonutRelation
+    {
+        return new DonutRelation(
+            Incident::query(),
+            $this,
+            $this->centroid_lat,
+            $this->centroid_lng,
+            0.5,   // inner radius km
+            2.0    // outer radius km
+        );
+    }
 }
