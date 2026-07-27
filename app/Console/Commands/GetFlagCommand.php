4067e265b325c8be0597bc92c212dded9b1f435c
marouane-tabib <tabibmarouane6@gmail.com>
2026-07-27 20:02:34 +0100

    database_export | Make GetFlagCommand lass and Add handler process, to get flag, from incidents in specific erea

---
+++ app/Console/Commands/GetFlagCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Neighborhood;
+use Illuminate\Console\Command;
+
+class GetFlagCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'flag:get';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Find and output the flag from incidents around NB-7A2F';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $neighborhood = Neighborhood::where('name', 'NB-7A2F')->firstOrFail();
+        $incidents = $neighborhood->incidents()->getResults();
+
+        $flag = $incidents->pluck('code')->implode('');
+
+        $this->info("Flag: " . $flag);
+    }
+}
